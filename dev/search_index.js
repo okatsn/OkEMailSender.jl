e8d23b3d5b49663cae45e7afddac4099aee7041e
Documenter.jl <documenter@juliadocs.github.io>
2024-06-25 06:17:14 +0000

    build based on d034b76

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkEMailSender","category":"page"},{"location":"#OkEMailSender","page":"Home","title":"OkEMailSender","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkEMailSender.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkEMailSender]","category":"page"}]
+}
